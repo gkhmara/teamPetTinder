772e9550d8db0a0bb220db640c8468f8b9da3f5e
Greg Khmara <gkhmara@gmail.com>
2022-03-29 20:15:10 -0700

    update files to display name, description, and photo
    Co-authored-by: Rosie Gardner <rosiegardner78@gmail.com>
    Co-authored-by: Justice Douglas <justice.douglas@hey.com>
    Co-authored-by: Deya Card <thedeyacard@gmail.com>

--- src/index.js
+++ src/index.js
@@ -12,19 +12,30 @@ console.log($);
 
 // console.log(animalArray);
 
-function getElements(data, children) {
-  let output = children;
-  console.log(output);
-  console.log(data);
-  $('.showResults').text(data.animals[0].name);
+// function getElements(data) {
+//   console.log(data);
+//   console.log(data.animals.length);
+//   $('.showResults').text(data.animals[1].name);
+// }
+
+function clearFields() {
+  $('.showResults').text("");
+}
+
+function getElements(data) {
+  for (let i = 0; i < data.animals.length; i++) {
+    $('.showResults').append(`<img src="${data.animals[i].primary_photo_cropped.small}">` + data.animals[i].name + ": " + data.animals[i].description + `<br>`);
+    console.log(data);
+  }
 }
 
 $(document).ready(function() {
   $('#enterSearch').click(function() {
     event.preventDefault();
     const children = $('input:radio[name=children]:checked').val();
+    clearFields();
     keyCall.getAccess(children).then(function(data) {
-      getElements(data, children); 
+      getElements(data); 
     });
     //    console.log(children);
     //    $('.showResults').text(children);
