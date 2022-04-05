e5633d5f83b4ade6d457cce14e66db61ad5b27a6
Rosie Gardner <rosiegardner78@gmail.com>
2022-04-04 21:03:13 -0700

    UPDATE: function getElements functionality

--- src/index.js
+++ src/index.js
@@ -24,10 +24,28 @@ function clearFields() {
 function getElements(data) {
   for (let i = 0; i < data.animals.length; i++) {
     if (data.animals[i].photos.length < 0) {
-      $('.showResults').append(data.animals[i].name + ": " + data.animals[i].breeds.primary + `<br>`+ data.animals[i].age + `<br>`);
+      $('.showResults').append(data.animals[i].name + ": " + data.animals[i].breeds.primary + `<br>` + data.animals[i].age + `<br>`);
       // console.log(data, data.animals[i].photos.length);
     } else if (data.animals[i].photos.length > 0)
-      $('.showResults').append(`<a href="${data.animals[i].url}"><img src="${data.animals[i].primary_photo_cropped.small}"></a>` + `<ul>` + "<b>Name</b>: " + data.animals[i].name + `<br>` + "<b>Breed</b>: " + data.animals[i].breeds.primary + `<br>` + "<b>Age</b>: " + data.animals[i].age + `<br>` + "<b>Gender</b>: " + data.animals[i].gender + `<br>` + "<b>Location</b>: " + data.animals[i].contact.address.city + ", " + data.animals[i].contact.address.state + `<br>` + "<b>Click Image for Adoption Details</b>" + `<br>`);
+      $('.showResults').append(
+        `<div class="card border-dark mb-3" style="width: 25rem;">` 
+        + `<img class="card-img-top"<img src="${data.animals[i].primary_photo_cropped.small}"></a>` 
+        + `<ul>`
+        + `<div class="card-body">` 
+        + `<h5 class="card-title">Name: ${data.animals[i].name}</h5>` 
+        + `<b class=text-primary>Breed</b>: ${data.animals[i].breeds.primary}` 
+        + `<br>` 
+        + `<b class=text-primary>Age</b>: ${data.animals[i].age}`
+        + `<br>` 
+        + `<b class=text-primary>Gender</b>: ${data.animals[i].gender}` 
+        + `<br>` 
+        + `<b class=text-primary>Location</b>: ${data.animals[i].contact.address.city},${data.animals[i].contact.address.state}`
+        + `<br>` 
+        + `<a href="${data.animals[i].url}" type="button" class="btn btn-info">Click for Info</a>`
+        + `<br>` 
+        + `</div>`
+        + `</div>`
+      );
     // console.log("LOCATION = " + data.animals[i].contact.address.city + data.animals[i].contact.address.state);
   }  
 }
