0aedc29b29bdd8426618ad55e993d37f73d48ef2
Rosie Gardner <rosiegardner78@gmail.com>
2022-04-05 20:38:42 -0700

    business.js ready to push

--- src/js/business.js
+++ src/js/business.js
@@ -1,7 +1,7 @@
 
 export default class keyCall {
 
-  static async getAccess(species, children, dogs, cats, special) {
+  static async getAccess(species, children, dogs, cats, city, state) {
     const key = process.env.API_KEY;
     const secret = process.env.SECRET;
 		
@@ -21,17 +21,14 @@ export default class keyCall {
       // Log the API data
       console.log('token', data);
 
-      // API Call Example - Justice: https://api.petfinder.com/v2/animals?types=cat&types=dog&page=2&limit=5&good_with_children=false&good_with_dogs=false
       // Return a second API call
       // This one uses the token we received for authentication
-      return fetch(`https://api.petfinder.com/v2/animals?type=${species}&page=2&limit=25&special_needs=${special}&good_with_children=${children}&good_with_dogs=${dogs}&good_with_cats=${cats}&status=adoptable`, {
+      return fetch(`https://api.petfinder.com/v2/animals?location=${city}, ${state}&type=${species}&page=2&limit=25&good_with_children=${children}&good_with_dogs=${dogs}&good_with_cats=${cats}&status=adoptable`, {
         headers: {
           'Authorization': data.token_type + ' ' + data.access_token,
           'Content-Type': 'application/x-www-form-urlencoded'
         }
       });
-
-      // &location=${state}
 
     }).then(function (resp) {
 
